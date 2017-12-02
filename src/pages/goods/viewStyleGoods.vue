20bf270daa2bee0bad08b1e2cd6847846e38d981
songStar <1043328710@qq.com>
2017-12-02 22:44:49 +0800

    💥 更新 viewStyleGoods

--- src/pages/goods/viewStyleGoods.vue
+++ src/pages/goods/viewStyleGoods.vue
@@ -13,16 +13,30 @@
     <el-row type="flex" class="row-bg" justify="space-around">
   <el-col :span="6" v-for="item in styleGoods" :key="item">
   <el-card :body-style="{ padding: '0px' }" class="wrap">
-  <div class="box" v-for="item2 in item.img">
-  		<img :src="item2.url" alt="">
-  	</div>
+    <el-carousel height="300px">
+      <el-carousel-item v-for="(item2,index) in item.img" :key="index">
+        <img :src="item2.url" alt="">
+      </el-carousel-item>
+    </el-carousel>
   	<div style="padding: 14px;">
-       <div class="name">{{item.name}}</div>
-       <el-row type="flex" class="row-bg" justify="space-around">
-  <el-col :span="12"><router-link :to="{name: 'styleGoods', params: {item: item, gid: gid, sid: item.sid}}" tag="span"><el-button type="primary" size="small" icon="edit"></el-button></router-link></el-col>
-  <el-col :span="12"><el-button type="danger" size="small" icon="delete" @click="delete_data(item)"></el-button></el-col>
-   </el-row>
-      </div>
+       <div class="name" style="marginBottom:10px;">{{item.name}}</div>
+       <el-row v-for="(stock, size) in item.size" style="height:50px;">
+       <el-col :span="5" style="lineHeight:40px;">{{size}}</el-col>
+       <el-col :span="19" style="textAlign: right;">
+        <el-input-number v-model="item.size[size]"  size="small" :min="1" :max="99999" label="描述文字"></el-input-number>
+      </el-col>
+       </el-row>
+       <el-row type="flex" class="row-bg" justify="space-between">
+  <el-col :span="12">
+    <router-link :to="{name: 'styleGoods', params: {item: item, gid: gid, sid: item.sid}}" tag="span">
+    <el-button type="primary" size="small" icon="el-icon-edit"></el-button>
+    </router-link>
+  </el-col>
+  <el-col :span="12">
+    <el-button type="danger" size="small" icon="el-icon-delete" @click="delete_data(item)"></el-button>
+  </el-col>
+  </el-row>
+  </div>
   </el-card>
   	
   </el-col>
@@ -46,6 +60,14 @@ export default {
       batch_select: []
     }
   },
+  watch: {
+    styleGoods: {
+      handler (val) {
+        this.editStyleGoods(val)
+      },
+      deep: true
+    }
+  },
   mounted () {
     this.getStyleGoods()
   },
@@ -63,6 +85,9 @@ export default {
           this.$message.error(res.msg)
         }
       })
+    },
+    editStyleGoods (val) {
+      console.log(val)
     },
     // 刷新
     on_refresh () {
